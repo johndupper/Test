a012069848ec1394da063f5b658e201774d1e52d
John Dupper <john.dupper@me.com>
2017-02-05 15:17:14 -0500

    created nav partial for landing (welcome) page

--- scrape.rb
+++ scrape.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'nokogiri'
 
 doc = Nokogiri::HTML(open("https://newsapi.org/sources"))
+# api = Rails.application.secrets.news_api_key
 
 sources = doc.css('.source')
 
@@ -10,20 +11,25 @@ sources = doc.css('.source')
 # regex to extract the url?
 # puts img_text
 
+
 # each source title
 sources.each do |source|
   name = source.css('.logo').attribute('title')
   source_as_string = name.to_s
+
   # puts source_as_string
   news_url = source_as_string.gsub(/\s+/, "-")
   puts news_url
-  # puts "'https://newsapi.org/v1/articles?source=#{news_url}&apiKey=66944983190c4b47a72a43e3a8605e87'"
+
+  # how do i get the secret.yml api key here?
+  # puts "https://newsapi.org/v1/articles?source=#{news_url}&apiKey=#{api}"
 end
 
 
 # the difference between urls is this :: &sortBy=top
 # https://newsapi.org/v1/articles?source=the-telegraph&sortBy=top&apiKey=66944983190c4b47a72a43e3a8605e87
 # https://newsapi.org/v1/articles?source=The-Telegraph&apiKey=66944983190c4b47a72a43e3a8605e87
+
 
 # each source's article names
 
