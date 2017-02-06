380a1f0e64d63a295d0c69ff7e08710c28c87256
John Dupper <john.dupper@me.com>
2017-02-06 12:24:20 -0500

    seeding database complete, functional

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,26 +4,6 @@ class WelcomeController < ApplicationController
 
 
   def index
-    guardian_url = 'http://content.guardianapis.com/search?api-key=7b592822-659d-4434-94a6-0e2c6e3ed734'
-    guardian_response = HTTParty.get(guardian_url)
-    guardian_news_response = guardian_response.parsed_response
-    guardian_news = guardian_news_response["response"]["results"]
-    @guardian = guardian_news
-
-    tech_url = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=66944983190c4b47a72a43e3a8605e87"
-    tech_response = HTTParty.get(tech_url)
-    tech_news = tech_response.parsed_response
-    @tech_news = tech_news
-
-    cnn_url = 'https://newsapi.org/v1/articles?source=cnn&apiKey=66944983190c4b47a72a43e3a8605e87'
-    cnn_response = HTTParty.get(cnn_url)
-    cnn_news_response = cnn_response.parsed_response
-    cnn_news = cnn_news_response
-    @cnn_news = cnn_news
-
-    ap_url = 'https://newsapi.org/v1/articles?source=associated-press&apiKey=66944983190c4b47a72a43e3a8605e87'
-    ap_response = HTTParty.get(ap_url)
-    ap_news = ap_response.parsed_response
-    @ap = ap_news
+    @sources = Source.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Source.create(name: "tech_news",
-              url: "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=66944983190c4b47a72a43e3a8605e87",
-              img_url: "http://i.newsapi.org/techcrunch-m.png")
+# Source.create(name: "tech_news",
+#               url: "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=66944983190c4b47a72a43e3a8605e87",
+#               img_url: "http://i.newsapi.org/techcrunch-m.png")
+#
+# Source.create(name: "cnn_news",
+#               url: "https://newsapi.org/v1/articles?source=cnn&apiKey=66944983190c4b47a72a43e3a8605e87",
+#               img_url: "http://i.newsapi.org/cnn-m.png")
+#
+# Source.create(name: "ap_news",
+#               url: "https://newsapi.org/v1/articles?source=associated-press&apiKey=66944983190c4b47a72a43e3a8605e87",
+#               img_url: "http://i.newsapi.org/associated-press-m.png")
 
-Source.create(name: "cnn_news",
-              url: "https://newsapi.org/v1/articles?source=cnn&apiKey=66944983190c4b47a72a43e3a8605e87",
-              img_url: "http://i.newsapi.org/cnn-m.png")
 
-Source.create(name: "ap_news",
-              url: "https://newsapi.org/v1/articles?source=associated-press&apiKey=66944983190c4b47a72a43e3a8605e87",
-              img_url: "http://i.newsapi.org/associated-press-m.png")
-
-##############################################################
+# we have the data we need to dynamically create our seeds
 
 require 'httparty'
 require 'open-uri'
@@ -27,47 +28,33 @@ require 'nokogiri'
 doc = Nokogiri::HTML(open("https://newsapi.org/sources"))
 news_source_list = doc.css('.source')
 
-available_news = []
-
-# cycle through html list of sources
-# check url for valid api GET path
+# available_news = []
 
 news_source_list.each do |news_source|
   name = news_source.css('.logo').attribute('title')
-  # model: title
   name_as_string = name.to_s
-
 
   news_url_ready = name_as_string.gsub(/\s+/, "-").downcase
   url = "https://newsapi.org/v1/articles?source=#{news_url_ready}&sortBy=top&apiKey=66944983190c4b47a72a43e3a8605e87"
   api_response = HTTParty.get(url).parsed_response
 
-
   if api_response["status"] == "ok"
-    # model: api url
-    puts "#{name_as_string} has #{api_response["articles"].length} articles."
-    available_news.push(news_url_ready)
 
     image = news_source.css('.logo').attribute('style')
-    image_string = image.to_s
+    image_string = image.to_s.sub('background-image:url(', '').chomp(')')
+
+    Source.create(
+      name: name_as_string,
+      url: url,
+      img_url: image_string
+    )
+
+    # available_news.push(source)
+
     # model: news logo
-    puts image_string.sub('background-image:url(', '').chomp(')')
+    # puts "News Image URL: #{image_string.sub('background-image:url(', '').chomp(')')}\n"
+    # puts image_string.sub('background-image:url(', '').chomp(')')
   end
 end
 
-puts available_news.length
-
-
-
-
-
-
-# NEED TO KNOW:
-
-# api = Rails.application.secrets.news_api_key
-
-
-
-# link = source.attribute('href')
-# link_as_string = link.to_s
-# puts link_as_string
+# puts available_news
