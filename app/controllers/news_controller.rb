142e092b98f97b9ce67151e7cda1d51a3392288d
John Dupper <john.dupper@me.com>
2017-02-08 01:27:06 -0500

    need to get news sources from api

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,11 @@
+require 'httparty'
+require 'open-uri'
+
 class NewsController < ApplicationController
   def index
     if current_user
       @user_news_sources_count = Source.where(user_id: current_user.id).count
-      @user_news_sources   = Source.where(user_id: current_user.id)
+      @all_sources   = Source.where(user_id: current_user.id)
     end
-  end
-
-  def show
-    @input_id = params[:id]
   end
 end
