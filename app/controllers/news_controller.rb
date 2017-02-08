123dde92b2e848e392042bf45086194b411f6bf7
John Dupper <john.dupper@me.com>
2017-02-08 01:28:24 -0500

    wrong variable name in index route

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,7 +5,7 @@ class NewsController < ApplicationController
   def index
     if current_user
       @user_news_sources_count = Source.where(user_id: current_user.id).count
-      @all_sources   = Source.where(user_id: current_user.id)
+      @user_news_sources   = Source.where(user_id: current_user.id)
     end
   end
 end
