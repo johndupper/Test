5e11171e322c3a3fa0d65c3827107ef1ca53fc54
John Dupper <john.dupper@me.com>
2017-02-08 18:57:32 -0500

    migration rollback, non-persistant works better

--- db/migrate/20170208180809_add_news_to_sources.rb
+++
@@ -1,5 +0,0 @@
-class AddNewsToSources < ActiveRecord::Migration[5.0]
-  def change
-    add_column :sources, :news, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208180809) do
+ActiveRecord::Schema.define(version: 20170207004308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170208180809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "news"
   end
 
   create_table "users", force: :cascade do |t|
