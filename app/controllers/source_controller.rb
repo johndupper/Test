980dc91401517f37e4df166d9ae7adb6d8497727
John Dupper <john.dupper@me.com>
2017-02-09 14:35:29 -0500

    sms not available currently

--- app/controllers/source_controller.rb
+++ app/controllers/source_controller.rb
@@ -1,11 +1,9 @@
 class SourceController < ApplicationController
   load_and_authorize_resource  only: [:edit, :update, :destroy]
 
-  # refactor to clean up url
   def show
       @all_sources = Source.all.order(:name)
       @user_sources = @all_sources.where(user_id: current_user.id)
-
       if params[:id] != "show"
         @change_source = Source.find_by_id(params[:id])
         if @change_source.user_id == nil
