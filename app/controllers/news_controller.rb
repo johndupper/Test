4e471c80b142c4a5fa28fee1905df929f46f8e48
John Dupper <john.dupper@me.com>
2017-02-09 18:10:41 -0500

    changing styling

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,30 +4,15 @@ require 'open-uri'
 class NewsController < ApplicationController
   def index
     @sources = Source.where(user_id: current_user.id)
-    # loop through user's sources
+
     @sources.each do |news_source|
-      # get news articles per source
       api_response = HTTParty.get(news_source.url).parsed_response
 
-      # if response is good
       if api_response["status"] == "ok"
-
-        # turn api response into array of articles
+        # turn json response into array of news articles
         articles = api_response["articles"]
-        puts articles.class
         news_source.article = articles
-        # puts news_source.articles = api_response["articles"]
-
-        # loop through articles
-        # articles = []
-        # news_array.each do |story|
-          # array of headlines
-          # articles.push(story)
-      # end
-
-        # news_source.articles = headlines
-
-      end # :: if status
-    end   # :: each loop
+      end
+    end
   end
 end
--- app/controllers/source_controller.rb
+++ app/controllers/source_controller.rb
@@ -1,18 +1,22 @@
 class SourceController < ApplicationController
   load_and_authorize_resource  only: [:edit, :update, :destroy]
 
-  # refactor to clean up url
   def show
       @all_sources = Source.all.order(:name)
       @user_sources = @all_sources.where(user_id: current_user.id)
 
+      # if url is passed a news source
       if params[:id] != "show"
         @change_source = Source.find_by_id(params[:id])
+
+        # add/remove news source from user's list
         if @change_source.user_id == nil
           @change_source.update(user_id: current_user.id)
         else
           @change_source.update(user_id: nil)
         end
+
+        # re-render same page
         redirect_to :back
       end
   end
