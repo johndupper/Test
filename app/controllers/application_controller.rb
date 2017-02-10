1119df40ce7a5185638c46ef4bbe2734e76ca8fd
John Dupper <john.dupper@me.com>
2017-02-09 22:15:06 -0500

    styled form layout for signup, log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   include CanCan::ControllerAdditions
-  # flash[:notice] = "How did you get here?"
-  # flash.keep(:notice)
-  # redirect_to root_url
-  # end
 end
