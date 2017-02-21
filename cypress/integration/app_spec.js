a04a4f08b6e8f2e5927011a67dd7f0ba648366ce
John Dupper <john.dupper@me.com>
2017-02-21 15:35:11 -0500

    initial page load tests include url redirect

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,29 +1,42 @@
 
 // RELEVANT TESTING
-describe('relevant', function() {
+Cypress.config('baseUrl', 'http://localhost:3000')
 
+describe('initial page load', function() {
     beforeEach(function () {
         // reset state, local storage, clear cookies
     })
 
-    // page title
-    it('should have correct <title>', function() {
+    it(' /news should redirect to sign in URL', function() {
+        // redirect
+        cy.visit('/')
+            .url().should('include', '/users/sign_in')
+    })
+
+    it('/sources should redirect to sign in URL', function() {
+        cy.request({
+            url: '/source/show',
+            followRedirect: false
+        }).then(function(res) {
+            expect(res.status).to.eq(302)
+            expect(res.redirectedToUrl).to.eq('http://localhost:3000/users/sign_in')
+        })
+    })
+
+    it('should have correct title', function() {
         cy.visit('http://localhost:3000')
-        // subject        chaining      value
         cy.title().should('include', 'Relevant')
     })
 
-    // navigation elements
     it('should contain nav elements', function() {
-        // first (chaining)
+        // first element (chaining)
         cy.get('#navbar').find('ul').children('li')
             .find('a').first().should('contain', 'My News')
-
-        // second
+        // second element
         cy.get('#navbar').contains('Manage Sources')
     })
 
-    // signup, login
-    // it('should prompt for login', function () {})
+    it('should show log in form', function() {})
+})
 
-}) // end testing
+
